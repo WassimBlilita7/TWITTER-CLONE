78edb1e8b565c0222395a49ec963f9fd365c7c58
wassimblilita7 <wassim.blilita19@gmail.com>
2024-11-14 11:34:24 +0100

    1- auth controller added

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,54 @@
+import User from '../models/userModel.js';
+import bcrypt from 'bcryptjs';
+
 export const signup = async (req , res)=>{
-    res.json({
-        data : "You hit the signup endpoint"
-    });
+    try{
+        const {fullName , userName , email , password} = req.body;
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!emailRegex.test(email)){
+            return res.status(400).json({error : "Invalid Format Email"});
+        }
+        const existingUser = await User.findOne({username})
+        if(existingUser){
+            return res.status(400).json({error : "Username is already taken"});
+        }
+        const existingEmail = await User.findOne({email})
+        if(existingEmail){
+            return res.status(400).json({error : "Email is already taken"});
+        }
+
+        const salt  = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password ,salt);
+
+        const newUser = new User({
+            fullName ,
+            userName ,
+            email ,
+            password : hashPassword
+        });
+
+        if(newUser){
+            generateTokenAndSetCookie(newUser._id,res)
+            await newUser.save();
+
+            res.status(201).json({
+                _id : newUser._id,
+                fullName : newUser.fullName,
+                userName : newUser.userName,
+                email : newUser.email,
+                followers : newUser.followers,
+                following : newUser.following,
+                profilImg : newUser.profilImg,
+                coverImg : newUser.coverImg
+            });
+        }else{
+            res.status(400).json({error : "Invalid User Data"});
+        }
+    }catch(e){
+        res.status(500).json({error : "Internal Server Error"});
+
+    }
+
 }
 
 export const login = async (req , res)=>{
--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
+app.use(express.json());
+app.use(express.urlencoded({extended : true})) // to parse form data
 app.use("/api/auth" , authRoutes);
 
 app.listen(PORT, ()=>{
