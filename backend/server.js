754781d19f90b33baf059127d76346646a58f926
wassimblilita7 <wassim.blilita19@gmail.com>
2024-11-17 19:42:31 +0100

    3.1 - update routes files and paths

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/authRoutes.js";
+import authRoutes from "./routes/authRoute.js";
 import connectMongoDB from "./db/connectMongoDb.js";
 import cookieParser from "cookie-parser";
-import userRoutes from "./routes/userRoutes.js";
-import postRoutes from "./routes/postRoutes.js";
+import userRoutes from "./routes/userRoute.js";
+import postRoutes from "./routes/postRoute.js";
 import {v2 as cloudinary} from "cloudinary";
 
 dotenv.config();
